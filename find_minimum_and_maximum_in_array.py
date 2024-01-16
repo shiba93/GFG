efdb24820215f28f9c8665d931abfe323f65e9e3
Shivaa Kumaar <57598424+shiba93@users.noreply.github.com>
2024-01-16 17:41:30 +0530

    Update find_minimum_and_maximum_in_array.py

--- find_minimum_and_maximum_in_array.py
+++ find_minimum_and_maximum_in_array.py
@@ -18,7 +18,7 @@
 #Your Task:  
 #You don't need to read input or print anything. Your task is to complete the function getMinMax() which takes the array A[] and its #size N as inputs and returns the minimum and maximum element of the array
 
-Solution:
+#Solution:
 
 def getMinMax( a, n):
     min_val = max_val = a[0]
